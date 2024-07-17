b74ad85f0bdcee6d8023021517a1378f4ede8827
TrushitMarviya <marviyatrushit0@gmail.com>
2024-07-17 10:52:01 +0530

    project-1

---
+++ JavaScript - PR - I/script.js
@@ -0,0 +1,18 @@
+let pic = document.getElementsByClassName("img")
+let index = 0 ;
+function slides (index){
+    for(let i = 0 ; i <pic.length;i++){
+        pic[i].style.display = "none";
+    }
+    pic[index].style.display = "block";
+}
+function show (){
+    setInterval(()=>{
+        slides(index)
+        index ++ ;
+        if(index == pic.length){
+            index = 0
+        }
+    },1500)
+}
+show();
